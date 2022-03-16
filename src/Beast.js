3623bfa9972502d02d7a7aa23ad3d85046cf9a23
Anthony Hill <anthonymhill23@gmail.com>
2022-03-15 22:56:43 -0400

    added code, pictures wont render tho

--- src/Beast.js
+++ src/Beast.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import Emoji from "./emoji";
+import { Col } from "react-bootstrap";
 
 
 
-class Beast extends Component {
+class Beast extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -10,17 +12,19 @@ class Beast extends Component {
     }
   }
 
-  handleClick = _ => {
-    this.setState({ favorite: this.state.favorite + 1 })
-  }
+  handleClick = () => {
+    this.setState({ favorite: this.state.favorite + 1 });
+  };
 
   render() {
+    console.log(this.state)
     return (
-      <>
+      <article>
         <h2>{this.props.title}</h2>
         <img src= {this.props.imageUrl} title={this.props.title} onClick={this.handleClick} alt={this.props.title} />
         <p>{this.props.description}</p>
-      </>
+        <button onClick={ this.handleClick}><Emoji emoji="partying-face"/></button><span>Favorites:{this.state.forever}</span>
+      </article>
 
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import Beast from "./Beast";
-
+import hornedarray from "./Provided.json"
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import data from   "./Provided.json"
+import "./App.css"
 
 
 class Main extends Component {
     render() {
+        let beast = []
+        this.props.data.forEach((hornedBeast, index) =>{
+            beast.push(
+                <Beast
+                    imgURL={hornedBeast.imgURL}
+                    title={hornedBeast.title}
+                    description={hornedBeast.description}
+                    keyword={hornedBeast.keyword}
+                    key={index}
+
+                />
+
+            )
+
+
+        } );
         return (
-            <>
-                <Beast imageUrl="https://via.placeholder.com/150" title= "beast 1" description="the first beast" />
-                <Beast imageUrl="https://via.placeholder.com/150" title="beast 2" description="the second beast" />
-            </>
+            <main>
+                {beast}
+            </main>
         )
     }
-}
+    
 
-export default Main;
+}
+export default Main
