343ff9594578f6e381646570a2357a4b4cac4fad
Anthony Hill <anthonymhill23@gmail.com>
2022-03-17 22:13:40 -0400

    added title and styling for code

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import HeaderTitle from "./HeaderTitle";
+import Search from "./Search.js";
+import Drop from "./Drop.js";
 
- class Header extends Component {
+export default class Header extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            click: <HeaderTitle welcome='Welcome! Vote for your favorite animal' intro="Click the 💘 button to favorite an image" />,
+        }
+    }
+
     render() {
         return (
-            <div className='Header'>
-                <h1>Welcome to the site with images of animals!</h1>
+            <div className='heading'>
+                <div>{this.state.click}</div>
+                <Search onchange={this.props.onchange} />
+                <Drop onchangetwo={this.props.onchangetwo} />
             </div>
         )
     }
 }
-export default Header;
