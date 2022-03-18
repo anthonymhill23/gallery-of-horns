1824fa869f4f83cc3739c8eee5d490d29a1c1b19
Anthony Hill <anthonymhill23@gmail.com>
2022-03-17 22:20:20 -0400

    added title  and heading

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            click: <HeaderTitle welcome='Welcome! Vote for your favorite animal' intro="Click the 💘 button to favorite an image" />,
+            click: <HeaderTitle welcome='Welcome! Vote for  your favorite Horned Beast !!!' intro="Click the 💘 button to favorite an image" />,
         }
     }
 
