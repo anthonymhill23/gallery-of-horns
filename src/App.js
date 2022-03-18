0c5d801767f8e069fff58b24fdc504051ea5ecf7
Anthony Hill <anthonymhill23@gmail.com>
2022-03-17 21:46:13 -0400

    Refectored code, got thigns to display properly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
+import React, { Component } from 'react'
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
+import data from '../src/Provided.json'
+import SelectedBeast from './Modal.js'
 
-import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import data from "./Provided.json"
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false,
+      selectedBeast: {},
+      favorite: 0,
+      search: "",
+      hornSelected: ""
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main 
-      data={data}
-      />
-      <Footer />
-    </div>
-  );
+  openModal = (selectedBeast, favorite) => {
+    this.setState({ showModal: true, selectedBeast: selectedBeast, favorite: favorite })
+  }
+
+  closeModal = _ => {
+    this.setState({ showModal: false })
+  }
+
+  onchange = (event) => {
+    const searching = event.target.value.toLowerCase();
+    this.setState({ search: searching });
+  }
+
+  onchangetwo = (event) => {
+    this.setState({ hornSelected: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header onchange={this.onchange} onchangetwo={this.onchangetwo} />
+        <Main data={data} modalTrue={this.openModal} searchData={this.state.search} hornSelected={this.state.hornSelected} />
+        <Footer />
+        <SelectedBeast showModal={this.state.showModal} closeModal={this.closeModal} selectedBeast={this.state.selectedBeast} favorite={this.state.favorite} />
+      </div>
+    )
+  }
 }
-
-export default App;
